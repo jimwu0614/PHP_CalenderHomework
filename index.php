43f9f0a5f225145e437154e6c246db7c7ac74338
chiachun wu <a0283137@gmail.com>
2022-05-23 20:45:27 +0800

    完成LOGIN頁面CSS

--- index.php
+++ index.php
@@ -5,41 +5,41 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>萬年曆登入</title>
-    <style>
-      *{
-        box-sizing: border-box;
-        text-align: center;
-      }
-      table{
-        margin: 10px auto;
-      }
-    </style>
+<link rel="stylesheet" href="./css/styleLogin.css">
 </head>
 <body>
-    <h1>登入</h1>
-
-    <form action="chklogin.php" method="post">
-        <table>
-            <tr>
-                <td>帳號:</td>
-                <td><input type="text" name="acc" id=""></td>
-                </tr>
-            <tr>
-                <td>密碼:</td>
-                <td><input type="password" name="pw" id=""></td>
-            </tr>
-        </table>
-        <div>
-            <input type="submit" value="登入">
-            <input type="reset" value="重置">
+  <section>
+    <div class="color"></div>
+    <div class="color"></div>
+    <div class="color"></div>
+    <div class="color"></div>
+    <div class="color"></div>
+    <div class="box">
+      <div class="container">
+        <div class="form">
+          <h2>Login</h2>
+          <form action="chklogin.php" method="post">
+            <div class="inputbox">
+              <input type="text" name="acc" id="" placeholder="123">
+            </div>
+            <div class="inputbox">
+              <input type="password" name="pw" id="" placeholder="321">
+            </div>
+            <div class="button">
+              <input type="submit" value="登入">
+            </div>
+            <div class="button">
+              <input type="reset" value="重置">
+            </div>
+            <p>預設帳號:123</p>
+            <p>預設密碼:321</p>
+            <p>密碼正確與否,導向兩個不同方法製作的萬年曆</p>
+            
+          </form>
         </div>
-
-        <span>
-          <p>預設帳號:123</p>
-          <p>預設密碼:321</p>
-        </span>
-    </form>
-    
+      </div>
+    </div>
+  </section>    
 </body>
 </html>
 
